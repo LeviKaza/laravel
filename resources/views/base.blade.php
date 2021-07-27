fc0047474126efcb2711c95a91859f2741f23315
LeviKaza <M.Jason.45@hotmail.fr>
2021-07-27 11:19:17 +0200

    first commit

---
+++ resources/views/base.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title')</title>
+    <!-- CSS only -->
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/@yield('css').css">
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Lien 1</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Lien 2</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+    </header>
+    <main>
+        @yield('content')
+    </main>
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('base')
+
+@section('content')
+<h1>Acceuil</h1>
+@endsection
+
+@section('css', 'home')
+
+@section('title', 'Acceuil')
